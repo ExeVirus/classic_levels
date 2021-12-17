44ed3bcffabf3e43120085b4e0f6cb641198a270
ExeVirus <ddsallad@gmail.com>
2021-12-16 23:00:12 -0500

    first working level

--- init.lua
+++ init.lua
@@ -1,18 +1,17 @@
-ll_runtime.register_level(dofile(minetest.get_modpath("player_api") .. "/api.lua")
-        minetest.get_modpath("classic_levels").."schemes/lady.mts", 
+ll_runtime.register_level(
+        minetest.get_modpath("classic_levels").."/schemes/lady.mts", 
         1, 
         "test", 
         {x=50,y=0,z=50}, 
         1, 
         "lady_assets_grass", 
-        table.concat(
-        {
+        table.concat({
             "formspec_version[3]",
             "size[8,8]",
             "position[0.5,0.5]",
             "anchor[0.5,0.5]",
             "no_prepend[]",
-            "bgcolor[","#F0F0F0FF",";both;#AAAAAA40]",
-            "hypertext[2,3.5;4,4.25;;<global halign=center color=",primary_c," size=32 font=Regular>Level 1<global halign=center color=",on_secondary_c," size=16 font=Regular>\n",
-        }, 
+            "bgcolor[","#F0F0F0FF",";both;#F0F0F0FF]",
+            "hypertext[2,3.5;4,4.25;;Level 1]",
+        }),
         theme)
